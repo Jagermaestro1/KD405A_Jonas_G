28dc92630143d2069d4b638703dced414dc505a0
Jagermaestro1 <Jonas@Jonass-MacBook-Air.local>
2016-01-21 17:56:38 +0100

    ups

--- Assignment_1/src/se/mah/KD405A/jg/Main.java
+++ Assignment_1/src/se/mah/KD405A/jg/Main.java
@@ -6,6 +6,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import java.awt.Color;
+import javax.swing.ScrollPaneConstants;
+import java.awt.Font;
 
 public class Main extends JFrame {
 
@@ -32,11 +43,75 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 720, 480);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(Color.GRAY);
+		setJMenuBar(menuBar);
+		
+		JMenu mnArkiv = new JMenu("Arkiv");
+		mnArkiv.setBackground(Color.GRAY);
+		menuBar.add(mnArkiv);
+		
+		JMenuItem mntmSkrivUt = new JMenuItem("Skriv ut");
+		mnArkiv.add(mntmSkrivUt);
+		
+		JMenuItem mntmAvsluta = new JMenuItem("Avsluta");
+		mnArkiv.add(mntmAvsluta);
+		
+		JMenu mnMedlem = new JMenu("Medlem");
+		mnMedlem.setBackground(Color.GRAY);
+		menuBar.add(mnMedlem);
+		
+		JMenuItem mntmNyMedlem = new JMenuItem("Ny Medlem");
+		mnMedlem.add(mntmNyMedlem);
+		
+		JMenuItem mntmHittaMedlem = new JMenuItem("Hitta Medlem");
+		mnMedlem.add(mntmHittaMedlem);
+		
+		JMenu mnHjlp = new JMenu("Hjälp");
+		mnHjlp.setBackground(Color.GRAY);
+		menuBar.add(mnHjlp);
+		
+		JMenuItem mntmHjlp = new JMenuItem("Hjälp");
+		mnHjlp.add(mntmHjlp);
+		
+		JMenuItem mntmOmProgrammet = new JMenuItem("Om programmet");
+		mnHjlp.add(mntmOmProgrammet);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JLabel lblNamn = new JLabel("Namn:");
+		lblNamn.setFont(new Font("Helvetica", Font.BOLD | Font.ITALIC, 15));
+		lblNamn.setForeground(Color.WHITE);
+		lblNamn.setBounds(231, 131, 61, 16);
+		contentPane.add(lblNamn);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(Main.class.getResource("/images/gym2.jpg")));
+		lblNewLabel.setBounds(0, 0, 720, 480);
+		contentPane.add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 200, 480);
+		contentPane.add(scrollPane);
+		
+		JList list = new JList();
 
+		list.setBackground(Color.BLACK);
+		list.setForeground(Color.WHITE);
+	
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Jonas Gabrielsson", "Robin Medin", "Tim Roth  \t", "Amanda Plummer  \t", "John Travolta  \t", "Samuel L. Jackson  \t", "Phil LaMarr  \t", "Frank Whaley  ", "Burr Steers  ", "Bruce Willis  ", "Ving Rhames  \t", "Paul Calderón  \t", "Rosanna Arquette  \t", "Bronagh Gallagher  \t", "Eric Stoltz  \t", "Uma Thurman  \t", "Michael Gilden  \t", "Steve Buscemi  \t", "Christopher Walken  ", "Angela Jones  \t", "Maria de Medeiros  \t", "Kathy Griffin ", "Duane Whitaker  ", "Peter Greene  ", "Stephen Hibbert  ", "Alexis Arquette  ", "Quentin Tarantino ", "Harvey Keitel  \t", "Julia Sweeney  \t"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		scrollPane.setViewportView(list);
+	}
 }
--- Assignment_7/src/se/mah/KD405A/jg/Main.java
+++ Assignment_7/src/se/mah/KD405A/jg/Main.java
@@ -6,10 +6,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JList;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.AbstractListModel;
+import javax.swing.JCheckBoxMenuItem;
 
 public class Main extends JFrame {
-
-	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -33,10 +46,48 @@ public class Main extends JFrame {
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnArkiv = new JMenu("Arkiv");
+		menuBar.add(mnArkiv);
+		
+		JMenuItem mntmSkrivUt = new JMenuItem("Skriv ut");
+		mnArkiv.add(mntmSkrivUt);
+		
+		JMenuItem mntmAvsluta = new JMenuItem("Avsluta");
+		mnArkiv.add(mntmAvsluta);
+		
+		JMenu mnMedlem = new JMenu("Medlem");
+		menuBar.add(mnMedlem);
+		
+		JMenuItem mntmNyMedlem = new JMenuItem("Ny medlem");
+		mnMedlem.add(mntmNyMedlem);
+		
+		JMenuItem mntmHittaMedlem = new JMenuItem("Hitta medlem");
+		mnMedlem.add(mntmHittaMedlem);
+		
+		JMenu mnHjlp = new JMenu("Hjälp");
+		menuBar.add(mnHjlp);
+		
+		JMenuItem mntmHjlp = new JMenuItem("Hjälp");
+		mnHjlp.add(mntmHjlp);
+		
+		JMenuItem mntmOmProgrammet = new JMenuItem("Om programmet");
+		mnHjlp.add(mntmOmProgrammet);
+		getContentPane().setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 6, 207, 244);
+		getContentPane().add(scrollPane);
+		
+		JCheckBoxMenuItem chckbxmntmTest = new JCheckBoxMenuItem("test");
+		scrollPane.setRowHeaderView(chckbxmntmTest);
+		
+		textField = new JTextField();
+		textField.setBounds(225, 6, 219, 250);
+		getContentPane().add(textField);
+		textField.setColumns(10);
 	}
-
 }
