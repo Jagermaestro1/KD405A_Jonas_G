0c445b68a5e84194d7b0f9be26d859e91928ada3
Jagermaestro1 <Jonas@Jonass-MacBook-Air.local>
2016-01-27 13:31:55 +0100

    uppgift 1

--- Assignment_2/src/se/mah/KD405A/jg/House.java
+++ Assignment_2/src/se/mah/KD405A/jg/House.java
@@ -1,9 +1,33 @@
 package se.mah.KD405A.jg;
 
 public class House {
-
-	public House() {
-		// TODO Auto-generated constructor stub
+	private int yearBuilt;
+	private int size;
+	private final int minSize = 10;
+	private final int maxSize = 1000;
+	private final int yearOld = 1800;
+	private final int yearNew = 2015;
+	
+	public House(int yearBuilt,int size){
+		//this.yearBuilt = yearBuilt;
+		if(size>=minSize && size<=maxSize){
+			this.size = size;
+		}else{
+			this.size=0;
+		}
+		if(yearBuilt>=yearOld && yearBuilt<=yearNew){
+			this.yearBuilt = yearBuilt;
+		}else{
+			this.yearBuilt = 0;
+		}		
+	}
+	
+	public int getYearBuilt(){
+	return this.yearBuilt;
+	}
+	
+	public int getSize(){
+		return this.size;
+	} 
 	}
 
-}
