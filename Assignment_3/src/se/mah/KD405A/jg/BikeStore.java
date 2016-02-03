6702c47043934c4b36096f0063fd28bdcb02485d
Jagermaestro1 <Jonas@jonass-air.clients.internal.mah.se>
2016-02-03 18:09:09 +0100

    something

--- Assignment_3/src/se/mah/KD405A/jg/BikeStore.java
+++ Assignment_3/src/se/mah/KD405A/jg/BikeStore.java
@@ -1,5 +1,27 @@
 package se.mah.KD405A.jg;
 
-public class BikeStore {
+import java.util.ArrayList;
 
+public class BikeStore {
+	
+	String color;
+	int size;
+	int price;
+
+	private ArrayList<Bike> lagret = new ArrayList<Bike>();
+	
+	
+	public String getAllBikes(){
+
+		for (Bike c : lagret ) {
+		
+				
+			String s;
+			s= c.getColor() + c.getPrice() + c.getSize();	
+		
+	
+		return s;	
+	}
+		return null;
+}
 }
