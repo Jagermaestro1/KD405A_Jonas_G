b66f6a4a30c59146f37d3d1bb0539d3e1ab56703
Jagermaestro1 <Jonas@jonass-air.clients.internal.mah.se>
2016-02-05 09:27:27 +0100

    Uppgift 6

--- Assignment_3/src/se/mah/KD405A/jg/BikeStore.java
+++ Assignment_3/src/se/mah/KD405A/jg/BikeStore.java
@@ -3,25 +3,19 @@ package se.mah.KD405A.jg;
 import java.util.ArrayList;
 
 public class BikeStore {
-	
-	String color;
-	int size;
-	int price;
 
 	private ArrayList<Bike> lagret = new ArrayList<Bike>();
-	
-	
-	public String getAllBikes(){
 
-		for (Bike c : lagret ) {
-		
-				
-			String s;
-			s= c.getColor() + c.getPrice() + c.getSize();	
-		
-	
-		return s;	
+	public String getAllBikes() {
+		String s = "";
+
+		for (Bike c : lagret) {
+			s = s + c.getColor() + c.getPrice() + c.getSize();
+
+		}
+		return s;
 	}
-		return null;
-}
+	public void addBike(String color, int size, int price){
+		lagret.add(new Bike(color, size,price));
+	}
 }
