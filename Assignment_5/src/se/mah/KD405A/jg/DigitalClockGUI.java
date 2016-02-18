2209269f0b5fce51af562741b2d8775d6d1bf28f
Jagermaestro1 <Jonas@Jonass-MacBook-Air.local>
2016-02-18 19:14:39 +0100

    snygg kod

--- Assignment_5/src/se/mah/KD405A/jg/DigitalClockGUI.java
+++ Assignment_5/src/se/mah/KD405A/jg/DigitalClockGUI.java
@@ -54,54 +54,52 @@ public class DigitalClockGUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("ALARM");
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 20));
 		lblNewLabel.setBounds(40, 6, 82, 31);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblHour = new JLabel("Hour:");
 		lblHour.setBounds(20, 128, 41, 16);
 		contentPane.add(lblHour);
-		
+
 		JLabel lblMinute = new JLabel("Minute:");
 		lblMinute.setBounds(20, 156, 61, 16);
 		contentPane.add(lblMinute);
-		
+
 		txtH = new JTextField();
 		txtH.setText("H");
 		txtH.setBounds(73, 123, 61, 26);
 		contentPane.add(txtH);
 		txtH.setColumns(10);
-		
+
 		txtM = new JTextField();
 		txtM.setText("M");
 		txtM.setBounds(73, 151, 61, 26);
 		contentPane.add(txtM);
 		txtM.setColumns(10);
-		
+
 		JButton btnSetAlarm = new JButton("Set Alarm");
 		btnSetAlarm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(txtH.getText().isEmpty() || txtM.getText().isEmpty()){
+				if (txtH.getText().isEmpty() || txtM.getText().isEmpty()) {
 					JOptionPane.showMessageDialog(null, "You have to set alarm!");
+				} else {
+					_setHour();
+					_setMinute();
+					if (hour > 12 || minute > 60) {
+						JOptionPane.showMessageDialog(null, "WRONG!!!");
+					} else {
+						clock.setAlarm(hour, minute);
+						lblAlarm.setText(txtH.getText() + " : " + txtM.getText());
+					}
 				}
-				else{
-				_setHour();
-				_setMinute();
-				if(hour > 12 || minute > 60)
-				{
-					JOptionPane.showMessageDialog(null, "WRONG!!!");
-				}
-				else{
-			    clock.setAlarm(hour, minute);
-			    lblAlarm.setText(txtH.getText() + " : " + txtM.getText());}
-			}
 			}
 		});
 		btnSetAlarm.setBounds(17, 184, 117, 29);
 		contentPane.add(btnSetAlarm);
-		
+
 		JButton btnClearAlarm = new JButton("Clear Alarm");
 		btnClearAlarm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -113,43 +111,44 @@ public class DigitalClockGUI extends JFrame {
 		});
 		btnClearAlarm.setBounds(17, 226, 117, 29);
 		contentPane.add(btnClearAlarm);
-		
+
 		lblTime = new JLabel("");
 		lblTime.setBackground(Color.BLACK);
 		lblTime.setForeground(Color.BLACK);
 		lblTime.setFont(new Font("DialogInput", Font.PLAIN, 17));
 		lblTime.setBounds(20, 90, 120, 30);
 		contentPane.add(lblTime);
-		
+
 		JLabel lblC = new JLabel("C");
 		lblC.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 30));
 		lblC.setBounds(60, 35, 31, 53);
 		contentPane.add(lblC);
-		
+
 		clock = new ClockLogic(this);
 	}
 
 	public void setTimeOnLabel(String time) {
 		lblTime.setText(time);
-		
+
 	}
 
 	public void activateAlarm(boolean activate) {
-		alarm=activate;
-		if(alarm==true){
+		alarm = activate;
+		if (alarm == true) {
 			JOptionPane.showMessageDialog(null, "RingRing");
 			clock.clearAlarm();
-			
+
 		}
 	}
-		private void _setHour(){
-			String text = txtH.getText();
-			hour = Integer.parseInt(text);
-		}
-		
-		private void _setMinute(){
-			String text = txtM.getText();
-			minute = Integer.parseInt(text);
+
+	private void _setHour() {
+		String text = txtH.getText();
+		hour = Integer.parseInt(text);
+	}
+
+	private void _setMinute() {
+		String text = txtM.getText();
+		minute = Integer.parseInt(text);
 	}
 
 }
